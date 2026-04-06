a928464eeaa766d3a4911eadc17fec3d904e8dad
minhtamtuhoc <minhtamhmp@gmail.com>
2026-04-06 09:58:52 +0700

    Add files via upload

---
+++ Session09/l3/l3.sql
@@ -0,0 +1,12 @@
+CREATE INDEX idx_products_category
+    ON Products(category_id);
+
+CLUSTER Products USING idx_products_category;
+
+CREATE INDEX idx_products_price
+    ON Products(price);
+
+SELECT *
+FROM Products
+WHERE category_id = 10
+ORDER BY price;
